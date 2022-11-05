a390024962949e9864068d11d3df787648f99fe8
Vivek Chandra <chandravivek52@gmail.com>
2022-11-05 20:50:14 +0530

    Left Triangle

---
+++ LeftTriangle.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class LefttTriangle {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 7; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print(" ");
+                System.out.print("*");
+            }
+            System.out.println("");
+        }
+    }
+}
